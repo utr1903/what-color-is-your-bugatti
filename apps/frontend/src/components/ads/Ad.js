c3249b5625c2e59ee162052ce7d564459850985a
Ugur Turkarslan <52721344+utr1903@users.noreply.github.com>
2023-12-02 12:49:39 +0100

    Created draft of the website (#4)
    * Add chart js
    
    * Clean up
    
    * Implement initial draft

---
+++ apps/frontend/src/components/ads/Ad.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Ad.css";
+
+const Ad = () => {
+  return (
+    <div className="ad">
+      <h1 className="h1-ad">Advertisement</h1>
+    </div>
+  );
+};
+
+export default Ad;
---
+++ apps/frontend/src/components/header/Header.js
@@ -0,0 +1,13 @@
+// Header.js
+import React from "react";
+import "./Header.css"; // Import the CSS file
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="h1">Let's face it pal...</h1>
+    </div>
+  );
+};
+
+export default Header;
